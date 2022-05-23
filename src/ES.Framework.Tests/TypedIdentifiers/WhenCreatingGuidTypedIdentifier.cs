a2de2ae9204f71f243cabdaf40de659f213ff15c
Thijs Cornelis <thijs.cornelis@allphi.eu>
2022-05-23 21:39:52 +0200

    add tests for creating TypedIdentifiers

---
+++ src/ES.Framework.Tests/TypedIdentifiers/WhenCreatingGuidTypedIdentifier.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace ES.Framework.Tests.TypedIdentifiers;
+
+public class WhenCreatingGuidTypedIdentifier
+{
+	[Fact]
+	public void WithStaticMethod_ShouldGenerateValue() {
+		//Act.
+		var id = GuidTypedIdentifier.CreateNew();
+
+		//Assert.
+		id.Should().NotBeNull();
+		id.Value.Should().NotBeNullOrWhiteSpace();
+		id.TypedValue.Should().NotBeEmpty();
+	}
+
+	[Fact]
+	public void WithPublicDefaultConstructor_ShouldSetValue() {
+		//Arrange.
+		var expectedId = Guid.Parse("{671F4357-057A-4BB3-A4D3-567F0A5E0054}");
+
+		//Act.
+		var id = new GuidTypedIdentifier(expectedId);
+
+		//Assert.
+		id.Should().NotBeNull();
+		id.Value.Should().NotBeNullOrWhiteSpace();
+		id.TypedValue.Should().Be(expectedId);
+	}
+}
---
+++ src/ES.Framework.Tests/TypedIdentifiers/WhenCreatingIntegerTypedIdentifier.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace ES.Framework.Tests.TypedIdentifiers;
+
+public class WhenCreatingIntegerTypedIdentifier
+{
+	[Fact]
+	public void WithStaticMethod_ShouldGenerateDefaultValue() {
+		//Act.
+		var id = IntegerTypedIdentifier.CreateNew();
+
+		//Assert.
+		id.Should().NotBeNull();
+		id.Value.Should().NotBeNull().And.NotBeEmpty();
+		id.TypedValue.Should().Be(0);
+	}
+
+	[Fact]
+	public void WithPublicDefaultConstructor_ShouldSetValue() {
+		//Arrange.
+		var expectedId = 9001;
+
+		//Act.
+		var id = new IntegerTypedIdentifier(expectedId);
+
+		//Assert.
+		id.Should().NotBeNull();
+		id.Value.Should().NotBeNullOrWhiteSpace();
+		id.TypedValue.Should().Be(expectedId);
+	}
+}
