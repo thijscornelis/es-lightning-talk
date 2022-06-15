6f16d5ea1adb9334374da94f2275fd526da0ea2c
Thijs Cornelis <thijs.cornelis@allphi.eu>
2022-06-15 09:18:55 +0200

    final

--- src/ES.Sample.API/Program.cs
+++ src/ES.Sample.API/Program.cs
@@ -1,6 +1,16 @@
-using ES.Sample.API.Controllers;
+using ES.Framework.Domain.Repositories.Design;
 using ES.Sample.API.Extensions;
+using ES.Sample.Application.Commands.ChangeName.V1;
+using ES.Sample.Application.Commands.Create.V1;
+using ES.Sample.Application.Commands.Deposit.V1;
+using ES.Sample.Application.Commands.Withdraw.V1;
+using ES.Sample.Domain.Aggregates;
+using ES.Sample.Domain.Projections;
+using ES.Sample.Domain.Repositories.Design;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,10 +34,92 @@ if(app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
-app.MapWriteEndpoints();
-app.MapReadEndpoints();
-
 await EnsureDatabaseAvailability(app);
+
+app.MapPost("api/bank-accounts", async (IMediator mediator, [FromBody] CreateCommand command, CancellationToken cancellationToken) => {
+	var result = await mediator.Send(command, cancellationToken);
+	return result.ToHttpResult();
+});
+app.MapPut("api/bank-accounts/{id}/name", async (IMediator mediator, [FromQuery] Guid id, [FromBody] ChangeNameCommand command, CancellationToken cancellationToken) => {
+	command = command with { BankAccountId = BankAccountId.CreateNew(id) };
+	var result = await mediator.Send(command, cancellationToken);
+	return result.ToHttpResult();
+});
+app.MapPut("api/bank-accounts/{id}/withdraw", async (IMediator mediator, [FromQuery] Guid id, [FromBody] WithdrawCommand command, CancellationToken cancellationToken) => {
+	command = command with { BankAccountId = BankAccountId.CreateNew(id) };
+	var result = await mediator.Send(command, cancellationToken);
+	return result.ToHttpResult();
+});
+app.MapPut("api/bank-accounts/{id}/deposit", async (IMediator mediator, [FromQuery] Guid id, [FromBody] DepositCommand command, CancellationToken cancellationToken) => {
+	command = command with { BankAccountId = BankAccountId.CreateNew(id) };
+	var result = await mediator.Send(command, cancellationToken);
+	return result.ToHttpResult();
+});
+
+app.MapGet("api/bank-accounts/{id}/statements",
+			  ([FromRoute] Guid id, IProjectionRepository repository) => {
+					var result = new Result<BankStatementProjection[]>();
+					var stopwatch = new Stopwatch();
+					try {
+						 stopwatch.Start();
+
+						 var bankAccountId = BankAccountId.CreateNew(id);
+						 var queryable = repository.Get<BankStatementProjection>(x => x.BankAccountId == bankAccountId);
+						 result.SetResult(queryable.ToArray());
+					}
+					catch(Exception e) {
+						 result.SetException(e);
+					}
+					finally {
+						 stopwatch.Stop();
+						 result.SetElapsedTime(stopwatch.Elapsed);
+					}
+
+					return result.ToHttpResult();
+			  });
+
+app.MapGet("api/bank-accounts/{id}",
+	([FromRoute] Guid id, IProjectionRepository repository) => {
+		 var result = new Result<BankAccountProjection>();
+		 var stopwatch = new Stopwatch();
+		 try {
+			  stopwatch.Start();
+
+			  var bankAccountId = BankAccountId.CreateNew(id);
+			  var queryable = repository.Get<BankAccountProjection>(x => x.BankAccountId == bankAccountId);
+			  result.SetResult(queryable.AsEnumerable().SingleOrDefault());
+		 }
+		 catch(Exception e) {
+			  result.SetException(e);
+		 }
+		 finally {
+			  stopwatch.Stop();
+			  result.SetElapsedTime(stopwatch.Elapsed);
+		 }
+
+		 return result.ToHttpResult();
+	});
+app.MapGet("api/bank-accounts/{id}/state",
+	async ([FromRoute] Guid id, IBankAccountRepository repository, CancellationToken cancellationToken) => {
+		 var result = new Result<BankAccountState>();
+		 var stopwatch = new Stopwatch();
+		 try {
+			  stopwatch.Start();
+
+			  var bankAccountId = BankAccountId.CreateNew(id);
+			  var bankAccount = await repository.GetAsync(bankAccountId, cancellationToken);
+			  result.SetResult(bankAccount.State);
+		 }
+		 catch(Exception e) {
+			  result.SetException(e);
+		 }
+		 finally {
+			  stopwatch.Stop();
+			  result.SetElapsedTime(stopwatch.Elapsed);
+		 }
+
+		 return result.ToHttpResult();
+	});
 
 app.Run();
 
